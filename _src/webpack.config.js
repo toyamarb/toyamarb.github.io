a46b4e5f9932a75b71487dd35e360ad70f9751dd
mugi-uno <mugi.uno@gmail.com>
2017-08-12 16:32:46 +0900

    add jsx support

--- _src/webpack.config.js
+++ _src/webpack.config.js
@@ -2,9 +2,18 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-  entry: './app.js',
+  entry: './entry.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, '../'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
   }
 };
