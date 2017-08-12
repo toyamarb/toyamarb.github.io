401025854bc5c8209f4366472c3092ba16d8a219
mugi-uno <mugi.uno@gmail.com>
2017-08-12 17:00:35 +0900

    format started_at

--- _src/RecentlyEvents.jsx
+++ _src/RecentlyEvents.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import moment from 'moment';
 
 export default class RecentlyEvents extends React.Component {
+
+  formatDate(dateString) {
+    return moment(dateString).format('YYYY/MM/DD');
+  }
+
   render() {
     return (
       <ul>
@@ -8,7 +14,7 @@ export default class RecentlyEvents extends React.Component {
           this.props.data.events.map((event, index) => (
             <li key={index}>
               <a target="_blank" href={event.event_url}>
-                {event.started_at}: {event.title}
+                {this.formatDate(event.started_at)}: {event.title}
                 <span className="icon is-small">
                   <i className="fa fa-external-link"></i>
                 </span>
