32b9190b097d4762742fbd095e2c5c9f4783ab86
mugi-uno <mugi-uno@users.noreply.github.com>
2024-12-09 14:21:15 +0000

    deploy: e9ccadf4d7898d500166ecfad9eed63942a05c8c

--- _next/static/w1pABtnXycz0hx0pKvjmD/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/572-b6da866235d9a39b.js","static/css/59883ae51bd42305.css","static/chunks/pages/index-5c677c2b475d4c73.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
